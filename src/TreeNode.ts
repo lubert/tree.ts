2d3fe9f4d5f3431c34756485d804a0847069412f
Lu Zhu <misterzhu@gmail.com>
2021-05-11 18:39:01 -0700

    Initial implementation

---
+++ src/TreeNode.ts
@@ -0,0 +1,143 @@
+/**
+ * @public
+ */
+export type SearchCallback<T> = (node: TreeNode<T>) => (boolean | void);
+
+/**
+ * @public
+ */
+export type SearchStrategy = 'pre' | 'post' | 'breadth';
+
+/**
+ * @public
+ */
+export class TreeNode<T> {
+  constructor(
+    public model: T,
+    public parent: TreeNode<T> | null = null,
+    public children: TreeNode<T>[] = []
+  ) {}
+
+  get index(): number {
+    if (!this.parent) return 0;
+    return this.parent.children.indexOf(this);
+  }
+
+  get hasChildren(): boolean {
+    return this.children.length > 0;
+  }
+
+  /**
+   * Add node as a child.
+   */
+  add(child: TreeNode<T>): TreeNode<T> {
+    child.parent = this;
+    this.children.push(child);
+    return child;
+  }
+
+  /**
+   * Remove current node and its children from the tree and return.
+   */
+  drop(): TreeNode<T> {
+    if (this.parent !== null) {
+      const idx = this.parent.children.indexOf(this);
+      this.parent.children.splice(idx, 1);
+      this.parent = null;
+    }
+    return this;
+  }
+
+  /**
+   * Returns a shallow-copy.
+   */
+  clone(): TreeNode<T> {
+    const node = new TreeNode<T>(this.model);
+    node.children = this.children.map((child) => {
+      const newChild = child.clone();
+      newChild.parent = node;
+      return newChild;
+    });
+    return node;
+  }
+
+  /**
+   * Returns list of nodes to the root
+   */
+  path(): TreeNode<T>[] {
+    const path: TreeNode<T>[] = [];
+    const addToPath = (node: TreeNode<T>) => {
+      path.unshift(node);
+      if (node.parent) addToPath(node.parent);
+    };
+    addToPath(this);
+    return path;
+  }
+
+  /**
+   * Iterates over a node's children and returns a new root node.
+   */
+  map<U>(callback: (obj: T) => U): TreeNode<U> {
+    const node = new TreeNode<U>(callback(this.model));
+    node.children = this.children.map((child) => {
+      const newChild = child.map(callback);
+      newChild.parent = node;
+      return newChild;
+    });
+    return node;
+  }
+
+  /**
+   * Breadth-first search, return true in the callback to end iteration.
+   */
+  breadth(callback: SearchCallback<T>): TreeNode<T> | null {
+    const queue: TreeNode<T>[] = [this];
+
+    while (queue.length) {
+      const node = queue.shift() as TreeNode<T>;
+      if (callback(node)) return node;
+      for (let i = 0, childCount = node.children.length; i < childCount; i++) {
+        queue.push(node.children[i]);
+      }
+    }
+
+    return null;
+  }
+
+  /**
+   * Depth-first pre-order search, return true in the callback to end iteration.
+   */
+  pre(callback: SearchCallback<T>): TreeNode<T> | null {
+    if (callback(this)) return this;
+
+    for (let i = 0, childCount = this.children.length; i < childCount; i++) {
+      const node = this.children[i].pre(callback);
+      if (node) return node;
+    }
+
+    return null;
+  }
+
+  /**
+   * Depth-first post-order search, return true in the callback to end iteration.
+   */
+  post(callback: SearchCallback<T>): TreeNode<T> | null {
+    for (let i = 0, childCount = this.children.length; i < childCount; i++) {
+      const node = this.children[i].post(callback);
+      if (node) return node;
+    }
+
+    if (callback(this)) return this;
+
+    return null;
+  }
+
+  /**
+   * Returns a list of nodes
+   */
+  flatten(method: SearchStrategy): TreeNode<T>[] {
+    const list: TreeNode<T>[] = [];
+    this[method].call(this, (node) => { list.push(node); });
+    return list;
+  }
+}
---
+++ tests/TreeNode.test.ts
@@ -0,0 +1,107 @@
+import { IParseable } from '../src/IParseable';
+import { Tree } from '../src/Tree';
+import { TreeNode, SearchStrategy } from '../src/TreeNode';
+
+describe('TreeNode', () => {
+  const tree: IParseable<string> = {
+    model: '1',
+    children: [{
+      model: '11',
+      children: [{
+        model: '111',
+        children: [],
+      }, {
+        model: '112',
+        children: [],
+      }],
+    }, {
+      model: '12',
+      children: [],
+    }]
+  };
+  const root = Tree.parse(tree);
+  const mapModel = <T>(node: TreeNode<T>, method: SearchStrategy) => {
+    return node.flatten(method).map((node) => node.model);
+  };
+
+  describe('.index', () => {
+    it('returns the expected index', () => {
+      expect(root.children[0].index).toEqual(0);
+      expect(root.children[1].index).toEqual(1);
+    });
+  });
+
+  describe('.hasChildren', () => {
+    it('returns the expected value', () => {
+      expect(root.hasChildren).toEqual(true);
+      expect(root.children[1].hasChildren).toEqual(false);
+    });
+  });
+
+  describe('.add', () => {
+    it('adds a new node', () => {
+      const node = new TreeNode<string>('1');
+      expect(mapModel(node, 'breadth')).toEqual(['1']);
+      const child = new TreeNode<string>('11');
+      node.add(child);
+      expect(mapModel(node, 'breadth')).toEqual(['1', '11']);
+      child.add(new TreeNode<string>('111'));
+      expect(mapModel(node, 'breadth')).toEqual(['1', '11', '111']);
+      node.add(new TreeNode<string>('12'));
+      expect(mapModel(node, 'breadth')).toEqual(['1', '11', '12', '111']);
+    });
+  });
+
+  describe('.drop', () => {
+    it('drops a node from the tree', () => {
+      const copy = root.clone();
+      const node = copy.children[0].children[0].drop();
+      expect(mapModel(copy, 'breadth')).toEqual(['1', '11', '12', '112']);
+      expect(mapModel(node, 'breadth')).toEqual(['111']);
+    });
+  });
+
+  describe('.clone', () => {
+    it('returns a shallow-copy', () => {
+      const clone = root.clone();
+      expect(mapModel(root, 'breadth')).toEqual(mapModel(clone, 'breadth'));
+    });
+  });
+
+  describe('.path', () => {
+    it('returns the correct path', () => {
+      expect(
+        root
+          .children[0]
+          .children[0]
+          .path()
+          .map((node) => node.model)
+      ).toEqual(['1', '11', '111']);
+    });
+  });
+
+  describe('.map', () => {
+    it('returns a transformed tree', () => {
+      const numTree = root.map((model) => parseInt(model));
+      expect(mapModel(numTree, 'breadth')).toEqual([1, 11, 12, 111, 112]);
+    });
+  });
+
+  describe('.breadth', () => {
+    it('traverses correctly', () => {
+      expect(mapModel(root, 'breadth')).toEqual(['1', '11', '12', '111', '112']);
+    });
+  });
+
+  describe('.pre', () => {
+    it('traverses correctly', () => {
+      expect(mapModel(root, 'pre')).toEqual(['1', '11', '111', '112', '12']);
+    });
+  });
+
+  describe('.post', () => {
+    it('traverses correctly', () => {
+      expect(mapModel(root, 'post')).toEqual(['111', '112', '11', '12', '1']);
+    });
+  });
+});
